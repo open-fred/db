d541cac0e6a8ce460ed4b43b672da8f65fb934af
Ludwig Huelk <ludwig.huelk@rl-institut.de>
2017-07-27 19:08:27 +0200

    add hydro gauge scripts

---
+++ data_import/environmental/waters/fred_pp_gaugehight.sql
@@ -0,0 +1,152 @@
+/*
+river gauge height preprocessing
+
+__copyright__   = "Reiner Lemoine Institut"
+__license__     = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__url__         = ""
+__author__      = "Ludee"
+*/
+
+DROP TABLE IF EXISTS    public.fred_gaugeheight_station;
+CREATE TABLE            public.fred_gaugeheight_station (
+    id          serial NOT NULL,
+    station_id  bigint,
+    station     text,
+    name        text,
+    plz         varchar(5),
+    water       text,
+    kilometer   double precision,
+    comment     text,
+    gk3_zone    integer,
+    rechts      bigint,
+    hoch        bigint,
+    geom        geometry(Point,3035),
+    CONSTRAINT fred_gaugeheight_station_pkey PRIMARY KEY (station_id));
+
+-- grant (rli_write)
+ALTER TABLE public.fred_gaugeheight_station OWNER TO rli_write;
+
+-- index GIST (geom)
+CREATE INDEX fred_gaugeheight_station_gidx
+    ON public.fred_gaugeheight_station USING GIST (geom);
+
+
+INSERT INTO public.fred_gaugeheight_station (station_id,station,water,kilometer,geom)
+    SELECT  station_id ::bigint,
+            station,
+            water,
+            kilometer,
+            geom
+    FROM    public.pegelonline_messpunkte_stammdaten
+    ORDER BY station_id;
+
+UPDATE public.fred_gaugeheight_station AS t1
+    SET     name = t2.name,
+            comment = t2.comment,
+            rechts = t2.rechts,
+            hoch = t2.hoch,
+            gk3_zone = t2.gk3_zone
+    FROM    public.fred_gaugeheight_location AS t2
+    WHERE   t1.station = UPPER(t2.name);
+
+-- metadata
+COMMENT ON TABLE public.fred_gaugeheight_station IS '{
+	"title": "Gauge height stations",
+	"description": "",
+	"language": [ "eng", "ger" ],
+	"spatial": 
+		{"location": "none",
+		"extent": "Germany",
+		"resolution": "vector"},
+	"temporal": 
+		{"reference_date": "2017",
+		"start": "none",
+		"end": "none",
+		"resolution": "none"},
+	"sources": [
+		{"name": "PEGELONLINE", "description": "PEGELONLINE publiziert mit verschiedenen Diensten tagesaktuelle Rohwerte unterschiedlicher gewässerkundlicher Parameter (z.B. Wasserstand) von Binnen- und Küstenpegeln der Wasserstraßen des Bundes bis maximal 30 Tage rückwirkend.", "url": "https://www.pegelonline.wsv.de/", "license": "", "copyright": ""},
+		{"name": "PEGELONLINE Web Feature Service Aktuell (WFS Aktuell)", "description": "PEGELONLINE WFS Aktuell stellt kostenfrei tagesaktuelle Wasserstände der Binnen- und Küstenpegel der Wasserstraßen des Bundes als WFS zur Verfügung. Hierbei können aktuelle Wasserstände der Pegel als Vektordaten bezogen werden und in eine Kartenanwendung integriert werden.", "url": "https://www.pegelonline.wsv.de/webservice/wfsAktuell", "license": "", "copyright": ""} ],
+	"license": 
+		{"id": "",
+		"name": "",
+		"version": "",
+		"url": "",
+		"instruction": "",
+		"copyright": ""},
+	"contributors": [
+		{"name": "LudEE", "email": "", "date": "2017-07-27", "comment": "Create table and metadata"},
+		{"name": "", "email": "", "date": "", "comment": ""} ],
+	"resources": [
+		{"name": "public.fred_gaugeheight_station",		
+		"format": "PostgreSQL",
+		"fields": [
+			{"name": "id", "description": "Unique identifier", "unit": "none"},
+			{"name": "station_id", "description": "Pegelnummer", "unit": "none"},
+			{"name": "station", "description": "Name des Pegels (groß)", "unit": "none"},
+            {"name": "name", "description": "Name des Pegels", "unit": "none"},
+            {"name": "plz", "description": "Postleitzahl", "unit": "none"},
+            {"name": "water", "description": "Name des Gewässers", "unit": "none"},
+            {"name": "kilometer", "description": "Flusskilometer", "unit": "none"},
+            {"name": "gk3_zone", "description": "GK3 Zone", "unit": "none"},
+            {"name": "rechts", "description": "GK3 Rechtswert (x)", "unit": "none"},
+            {"name": "hoch", "description": "GK3 Hochwert (y)", "unit": "none"},
+			{"name": "geom", "description": "Geometry", "unit": "none"} ] } ],
+	"metadata_version": "1.3"}';
+    
+    
+    
+    
+    
+    
+/* -- incomplete list from excel 
+DROP TABLE IF EXISTS    public.fred_gaugeheight_location;
+CREATE TABLE            public.fred_gaugeheight_location (
+    id          integer NOT NULL,
+    name        text,
+    plz         varchar(5),
+    river       text,
+    comment     text,
+    gk3_zone    integer,
+    rechts      bigint,
+    hoch        bigint,
+    geom        geometry(Point,3035),
+    CONSTRAINT fred_gaugeheight_location_pkey PRIMARY KEY (id));
+
+-- grant (rli_write)
+ALTER TABLE public.fred_gaugeheight_location OWNER TO rli_write;
+
+-- index GIST (geom)
+CREATE INDEX    fred_gaugeheight_location_gidx
+    ON  public.fred_gaugeheight_location USING GIST (geom);
+
+INSERT INTO public.fred_gaugeheight_location (id,name,river,comment,gk3_zone,rechts,hoch)
+    SELECT  id::int,
+            "Pegelmesser",
+            "Fluss",
+            "Quelle",
+            "GK3"::int,
+            rechts::int,
+            hoch::int
+    FROM    public.gauge_location
+    ORDER BY id;
+
+
+UPDATE public.fred_gaugeheight_location AS t1
+    SET geom = ST_TRANSFORM(ST_SetSRID(ST_MakePoint(t1.rechts,t1.hoch),31466),3035)
+    WHERE gk3_zone = '2';
+
+UPDATE public.fred_gaugeheight_location AS t1
+    SET geom = ST_TRANSFORM(ST_SetSRID(ST_MakePoint(t1.rechts,t1.hoch),31467),3035)
+    WHERE gk3_zone = '3';
+
+UPDATE public.fred_gaugeheight_location AS t1
+    SET geom = ST_TRANSFORM(ST_SetSRID(ST_MakePoint(t1.rechts,t1.hoch),31468),3035)
+    WHERE gk3_zone = '4';
+*/
+
+
+
+
+
+
+
