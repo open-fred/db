641aa0682734b39dffdfac4d7adc5504f9d69f95
KilianZimmerer <kikleer@altlautern.de>
2017-08-10 09:53:09 +0200

    update to metadata v1.3

--- data_import/noaa_globe_v1.3.sql
+++ data_import/noaa_globe_v1.3.sql
@@ -1,9 +1,4 @@
 /*
-
-------------------
-STILL IN PROGRESS
-------------------
-
 This script provides metadata for the 'environmental.noaa_globe' dataset
 A definition of metadata can be found in the openmod glossary http://wiki.openmod-initiative.org/wiki/Metadata
 A further description can be found on http://wiki.openmod-initiative.org/wiki/DatabaseRules
@@ -28,30 +23,31 @@ COMMENT ON TABLE environmental.noaa_globe IS '{
 	"language": [ "eng" ],
 	"reference_date": "1999-01-01",
 	"sources": [
-TODO		{"name": "National Geophysical Data Center, NOAA", "description": " ", "url": "http://www.ngdc.noaa.gov/mgg/topo/globe.html", "license": "No License Provided", "copyright": "© Reiner Lemoine Institut"} ],
+		{"name": "National Geophysical Data Center, NOAA", "description": "", "url": "http://www.ngdc.noaa.gov/mgg/topo/globe.html", "license": "", "copyright": ""},
+		{"name": "file", "description": "", "url": "https://www.ngdc.noaa.gov/mgg/topo/gltiles.html", "license": "", "copyright": ""} ],
 	"spatial": [
-		{"extent": "Global",
-TODO		"resolution": ""} ],
-TODO	"temporal": [
-		{"start": "2017-01-01",
-		"end": "2017-12-31",
-		"resolution": "hour"} ],
-TODO	"license": [
+		{"extent": "global",
+		"resolution": "0.0833deg x 0.0833deg"} ],
+T	"temporal": [
+		{"start": "",
+		"end": "",
+		"resolution": ""} ],
+	"license": [
 		{"id": "ODbL-1.0",
 		"name": "Open Data Commons Open Database License 1.0",
 		"version": "1.0",
 		"url": "https://opendatacommons.org/licenses/odbl/1.0/",
 		"instruction": "You are free: To Share, To Create, To Adapt; As long as you: Attribute, Share-Alike, Keep open!",
 		"copyright": "© Reiner Lemoine Institut"} ],
-TODO	"contributors": [
+	"contributors": [
 		{"name": "Ludee", "email": " ", "date": "2016-06-16", "comment": "Create metadata"},
 		{"name": "Ludee", "email": " ", "date": "2016-11-22", "comment": "Update metadata"},
 		{"name": "Ludee", "email": " ", "date": "2016-11-22", "comment": "Update header and license"},
 		{"name": "Ludee", "email": " ", "date": "2017-03-16", "comment": "Add license to source"},
 		{"name": "Ludee", "email": " ", "date": "2017-03-28", "comment": "Add copyright to source and license"},
-		{"name": "Ludee", "email": " ", "date": "2017-05-30", "comment": "Update metadata to version 1.3"} ],
-TODO	"resources": [
-		{"name": "model_draft.test_table",		
+		{"name": "KilianZimmerer", "email": " ", "date": "2017-08-10", "comment": "Update metadata to version 1.3"} ],
+	"resources": [
+		{"name": "environmental.noaa_globe",		
 		"format": "sql",
 		"fields": [
 			{"name": "id", "description": "Unique identifier", "unit": "" },
@@ -62,3 +58,30 @@ TODO	"resources": [
 
 -- select description
 SELECT obj_description('environmental.noaa_globe' ::regclass) ::json;
+
+-- index (rast)
+CREATE INDEX noaa_globe_rast_idx
+	ON environmental.noaa_globe USING GIST (ST_ConvexHull(rast));
+
+-- public.raster_columns
+SELECT AddRasterConstraints('environmental'::name, 'noaa_globe'::name, 'rast'::name, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE);
+
+-- grant
+ALTER TABLE environmental.noaa_globe
+	OWNER TO oeuser;
+
+-- MView (filter and projection)
+DROP MATERIALIZED VIEW IF EXISTS  	environmental.noaa_globe_germany_mview CASCADE;
+CREATE MATERIALIZED VIEW         	environmental.noaa_globe_germany_mview AS
+	SELECT	rid, ST_CLIP(ST_TRANSFORM(ng.rast,3035),vg.geom) AS rast
+	FROM	environmental.noaa_globe AS ng,
+		political_boundary.bkg_vg250_1_sta_bbox_mview AS vg
+	WHERE	vg.geom && ST_TRANSFORM(ST_ConvexHull(ng.rast),3035);
+
+-- index (rast)
+CREATE INDEX noaa_globe_germany_mview_rast_idx
+	ON environmental.noaa_globe_germany_mview USING GIST (ST_ConvexHull(rast));
+
+-- grant
+ALTER TABLE environmental.noaa_globe_germany_mview
+	OWNER TO oeuser;
