9e91272bf31a07acd783ee201422c1eeabd3a38f
KilianZimmerer <kikleer@altlautern.de>
2017-11-30 17:13:53 +0100

    importer

--- preprocessing/boundaries/osm_postcode/osm_postcode_importer.py
+++ preprocessing/boundaries/osm_postcode/osm_postcode_importer.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.insert(0, './../../../')
 
-from utility.io import download
+from utility.io import download, extraction
 
 # code execution
 # - the automatic download moves files to the target directory
@@ -9,13 +9,14 @@ from utility.io import download
 
 # download links. Assign 'None' for non-automatic download:
 links = None
-download_files = None # not possible to download https files
+download_files = ['plz_2stellig.zip'] # not possible to download https files
 schema = 'boundaries'
 tablename = 'osm_postcode'
 
 if links == None:
-    # (1) decompress files
+    # (1) decompress and extract files
     # (2) write to database and do things in between (in this case add 'stelle')
-    pass
+    extraction(download_files)
 else:
     download(links, download_files)
+    extraction(download_files)
