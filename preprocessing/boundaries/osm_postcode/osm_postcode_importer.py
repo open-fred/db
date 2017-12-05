4519d18a26b466cc142fac08d98776cb6ed8d2e0
KilianZimmerer <kikleer@altlautern.de>
2017-12-05 22:12:48 +0100

    importer

--- preprocessing/boundaries/osm_postcode/osm_postcode_importer.py
+++ preprocessing/boundaries/osm_postcode/osm_postcode_importer.py
@@ -8,22 +8,19 @@ from utility.io import download, extraction, write_to_db, database_session
 
 # links: Assign 'None' for non-automatic download:
 # not possible to download https files yet
-links = ['http://spatialkeydocs.s3.amazonaws.com/FL_insurance_sample.csv.zip']
-files = ['plz_2stellig.zip']
+links = None
+files = ['plz-1stellig.shp.zip']
 schema = 'boundaries'
-tablename = 'test'
+tablename = 'osm_postcode'
 conn = database_session(section='reiners_db')
 
-
 if links == None:
-    # (1) decompress and extract files
-    # (2) write to database and do things in between (in this case add 'stelle')
-    extraction(files)
+    pass
 else:
-#    download(links, files) #downloads all data from list 'links'
-    data_files1 = extraction(files[0]) # exctracts data_file from download files
-    print(data_files1)
-    write_to_db(data_files1[0], conn, tablename = tablename, schemaname = schema) # write data_file to database
-    # add 'stellen': here somethin can be added to this table
-    # data_file2 = extraction(files[1])
-    # ...
+    download(links, files)
+
+data_files1 = extraction(files[0]) # exctracts data_file from download files
+write_to_db(data_files1[0], conn, tablename = tablename, schemaname = schema) # write data_file to database
+# add 'stellen': here somethin can be added to this table
+# data_file2 = extraction(files[1])
+# ...
